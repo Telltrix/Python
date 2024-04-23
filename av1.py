a4e140305509838f0c3b9dee0f02ab54d8eddd56
Telltrix <telltrix@hotmail.com>
2024-04-23 16:33:19 -0300

    av1

--- av1.py
+++ av1.py
@@ -68,24 +68,23 @@
 # Faça um programa que solicite o valor da compra e a categoria de assinante (1, 2, 3 ou 4). 
 # Mostrar na tela o valor da compra de acordo com a opção escolhida.
 
-# freight= 12.5
-# valuePurchase= float(input('Digite o valor da compra: '))
-# category = input('Qual a categoria do assinante?\n1)Premium\n2)Gold\n3)Silver\n4)Não assinante: ')
+freight= 12.5
+valuePurchase= float(input('Digite o valor da compra: '))
+category = input('Qual a categoria do assinante?\n1-Premium\n2-Gold\n3-Silver\n4-Não assinante: ')
 
-# match category:
-#     case '1':
-#         freight = 0
-#         discount = 0.8
-#         print(f'Valor total: R${freight+(valuePurchase*discount):.2f}')
-#     case '2':
-#         discount = 0.8
-#         print(f'Valor total: R${freight+(valuePurchase*discount):.2f}')
-#     case '3':
-#         discount = 0.9
-#         print(f'Valor total: R${freight+(valuePurchase*discount):.2f}')
-#     case '4':
-#         discount = 1
-#         print(f'Valor total: R${freight+(valuePurchase*discount):.2f}')
-#     case _:
-#         discount = 0
-#         print('Opção inválida.')
+match category:
+    case '1':
+        freight = 0
+        discount = 0.8
+        print(f'Valor total: R${freight+(valuePurchase*discount):.2f}')
+    case '2':
+        discount = 0.8
+        print(f'Valor total: R${freight+(valuePurchase*discount):.2f}')
+    case '3':
+        discount = 0.9
+        print(f'Valor total: R${freight+(valuePurchase*discount):.2f}')
+    case '4':
+        discount = 1
+        print(f'Valor total: R${freight+(valuePurchase*discount):.2f}')
+    case _:
+        print('Opção inválida.')
