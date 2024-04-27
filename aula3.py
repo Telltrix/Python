d9fb36b0df54ceade607265c501e152a596421d0
TELMO GOMES DE AZEVEDO FILHO <telmo_azevedo-filho@estudante.sc.senai.br>
2024-04-27 12:01:08 -0300

    aula3

--- aula3.py
+++ aula3.py
@@ -31,30 +31,37 @@
 #     print(f'Numero: {number} é {parImpar}')
 # parImpar(int(input('Digite um numero: ')))
 
-def somar(number1, number2):
-    return number1+number2
+# def somar(number1, number2):
+#     return number1+number2
 
-def subtrair(number1, number2):
-    return number1-number2
+# def subtrair(number1, number2):
+#     return number1-number2
 
-def multiplicar(number1, number2):
-    return number1*number2
+# def multiplicar(number1, number2):
+#     return number1*number2
 
-def dividir(number1, number2):
-    return round(number1/number2,2)
+# def dividir(number1, number2):
+#     return round(number1/number2,2)
 
-def calculator(number1, number2):
-    operacao = input('Qual a operação entre os numeros quer realizar?\nDigite 1 para Adição\nDigite 2 para Subtração\nDigite 3 para Multiplicação\nDigite 4 para Divisão\n ')
-    match operacao:
-        case '1':
-            return somar(number1, number2)
-        case '2':
-            return subtrair(number1, number2)
-        case '3':
-            return multiplicar(number1, number2)
-        case '4':
-            return dividir(number1, number2)
-        case _:
-            return 'Opção inválida!'
+# def calculator(number1, number2):
+#     operacao = input('Qual a operação entre os numeros quer realizar?\nDigite 1 para Adição\nDigite 2 para Subtração\nDigite 3 para Multiplicação\nDigite 4 para Divisão\n ')
+#     match operacao:
+#         case '1':
+#             return somar(number1, number2)
+#         case '2':
+#             return subtrair(number1, number2)
+#         case '3':
+#             return multiplicar(number1, number2)
+#         case '4':
+#             return dividir(number1, number2)
+#         case _:
+#             return 'Opção inválida!'
 
-print(f'Resultado: {calculator(float(input('Digite o primeiro numero: ')), float(input('Digite o segundo numero: ')))}')
+# print(f'Resultado: {calculator(float(input('Digite o primeiro numero: ')), float(input('Digite o segundo numero: ')))}')
+
+def calcularPrecoTerreno(largura, comprimento):
+    precoMetro= float(1000)
+    return (largura*comprimento)*precoMetro
+print(f'Estimativa do valor do terreno de 50x20 é: R${calcularPrecoTerreno(50, 20):,.2f}')
+print(f'Estimativa do valor do terreno de 100x400 é: R${calcularPrecoTerreno(100, 400):,.2f}')
+print(f'Estimativa do valor do terreno de 20x1000 é: R${calcularPrecoTerreno(20, 1000):,.2f}')
